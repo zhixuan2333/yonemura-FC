9cf3770c4b961531b3422812f316eecf2331d142
zhixuan2333 <me@zhixuan.dev>
2025-03-27 10:11:35 +0900

    add router

---
+++ src/router.tsx
@@ -0,0 +1,17 @@
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import App from './App';
+
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />} />
+
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default Router;
