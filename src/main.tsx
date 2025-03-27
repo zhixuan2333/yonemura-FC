c17574993d58afe43793e08ffd518f7b2e2b8a46
zhixuan2333 <me@zhixuan.dev>
2025-03-27 10:11:27 +0900

    add router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+
+import Router from './router.tsx'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Router />
   </StrictMode>,
 )
